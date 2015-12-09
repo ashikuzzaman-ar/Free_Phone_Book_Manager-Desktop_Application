31b2f82bac0f21cbab08bf0be233369aea114e7c
ashik <ashik@ashik>
2015-12-10 00:27:44 +0600

    Calling MainFrame.launch() method to launch Main Interface of this software

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@
  */
 package main;
 
+import gui.MainFrame;
+
 /**
  *
  * @author ashik
@@ -14,7 +16,7 @@ public class Main {
     
     public static void main(String[] args) {
 
-        System.out.println("Testing Project......");
+        MainFrame.launch();
     }
     
 }
